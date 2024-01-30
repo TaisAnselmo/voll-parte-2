173369f8e9e7ef75cd872aa05f737cec2fe96571
tais anselmo <tais.ans2001@gmail.com>
2024-01-30 17:22:20 -0300

    web/package.json web/package.jsonweb/src/pages/PaginaInicial/Depoimentos/index.tsx

--- web/src/pages/PaginaInicial/Depoimentos/index.tsx
+++ web/src/pages/PaginaInicial/Depoimentos/index.tsx
@@ -32,10 +32,21 @@ export default function Depoimentos() {
     return (
         <Container>
             <Titulo>Depoimentos</Titulo>
-            <Texto>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</Texto>
+            <Texto>
+            O Ortopedista Dr. Silva demonstrou profundo conhecimento em sua área, 
+            explicando de maneira clara e detalhada sobre minha condição
+             ortopédica. Sua habilidade em transmitir informações complexas
+              de forma compreensível foi impressionante, proporcionando-me 
+              uma compreensão abrangente do meu problema.
+            </Texto>
             <Subtitulo>Gabriel, 26 anos, Olinda/PE.</Subtitulo>
             <Linha />
-            <Texto>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</Texto>
+            <Texto>Minha experiência com o Dr. Santos, infectologista, 
+            foi excepcional e digna de nota. Desde o momento em que 
+            entrei em seu consultório, percebi um ambiente acolhedor e 
+            uma equipe atenciosa. O Dr. Santos, com seu sorriso 
+            amigável, transmitiu uma sensação imediata de confiança.
+            </Texto>
             <Subtitulo>Taís, 22 anos, Recife/PE.</Subtitulo>
             <Linha />
             <Texto>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</Texto>
